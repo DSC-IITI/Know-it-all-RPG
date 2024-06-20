874c2fc5f6c8dc18c4d84bfa1d61644cb584de65
Mitanshu Kumawat <kumawatmitanshu0209@gmail.com>
2024-06-20 06:46:30 +0530

    Animations added

---
+++ Detective mystery/Assets/AnimationControlScript.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControlScript : MonoBehaviour
+{
+
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWalking = animator.GetBool("isWalking");
+        bool isRunning = animator.GetBool("isRunning");
+        bool talking = animator.GetBool("talk");
+        bool isShooting = animator.GetBool("isShooting");
+        bool ForwardPressed = Input.GetKey("w");
+        bool RightwardPressed = Input.GetKey("d");
+        bool LeftwardPressed = Input.GetKey("a");
+        bool BackwardPressed = Input.GetKey("s");
+        bool RunPressed = Input.GetKey("space");
+        bool speakEnabled = Input.GetKey("f");
+        bool Shoot = Input.GetButton("Fire1");
+
+        if(!isWalking && (ForwardPressed || RightwardPressed|| LeftwardPressed|| BackwardPressed)){
+            animator.SetBool("isWalking", true);
+        }
+        if(isWalking && !(ForwardPressed || RightwardPressed|| LeftwardPressed|| BackwardPressed)){
+            animator.SetBool("isWalking", false);
+        }
+
+        if(!isRunning && RunPressed && (ForwardPressed || RightwardPressed|| LeftwardPressed|| BackwardPressed)){
+            animator.SetBool("isRunning", true);
+        }
+        if(isRunning && (!RunPressed || (!ForwardPressed && !RightwardPressed &&!LeftwardPressed && !BackwardPressed))){
+            animator.SetBool("isRunning", false);
+        }
+
+        if(!talking && speakEnabled){
+            animator.SetBool("talk", true);
+        }
+        if(talking &&(!speakEnabled)){
+            animator.SetBool("talk", false);
+        }
+
+        if(Shoot && !isShooting){
+            animator.SetBool("isShooting", true);
+        }
+        if(!Shoot && isShooting){
+            animator.SetBool("isShooting", false);
+        }
+
+    }
+
+}
